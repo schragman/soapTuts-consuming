a666c870e2b17510af6c34564d8818631d8e5622
mschrage <michael.schrage@fincon.eu>
2022-07-12 07:58:53 +0200

    configuring pom.xml II and generated classes

---
+++ target/generated-sources/xjc/net/schrage/consumingwebservice/wsdl/package-info.java
@@ -0,0 +1,9 @@
+//
+// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.3.0 generiert 
+// Siehe <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
+// Generiert: 2022.07.12 um 07:57:50 AM CEST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://spring.io/guides/gs-producing-web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package net.schrage.consumingwebservice.wsdl;
